ad252a6e9c88ab21e8b43354cd05c4ec0bd8a947
Kristen Thyng <kristen@axds.co>
2021-06-03 11:10:42 -0500

    updated setup and init

--- extract_model/__init__.py
+++ extract_model/__init__.py
@@ -2,4 +2,11 @@
 Select model output using xarray and `isel` or interpolation through xarray or xesmf.
 """
 
+from pkg_resources import DistributionNotFound, get_distribution
+try:
+    __version__ = get_distribution("cf_xarray").version
+except DistributionNotFound:
+    # package is not installed
+    __version__ = "unknown"
+
 from .extract_model import get_var_cf, select
